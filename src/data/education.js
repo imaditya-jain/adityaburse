fe1f38011c07b703af7b762f1bcc7f8ed313579c
adityajain-web <60445026+adityajain-web@users.noreply.github.com>
2024-05-26 02:36:49 +0530

    empty

--- src/data/education.js
+++
@@ -1,7 +0,0 @@
-const education = [
-    { title: "BACHELOR OF ENGINEERING IN ELECTRONICS ENGINEERING", period: "2019", university: "Rashtrasant Tukodaji Maharaj Nagpur University" },
-    { title: "DIPLOMA IN ELECTRONICS ENGINEERING", period: "2015", university: "Maharashtra State Board Of Technical Education" },
-    { title: "SECONDARY SCHOOL CERTIFICATE", period: "2011", university: "Maharashtra State Board Of Secondary & Higher Secondary Education" },
-]
-
-export default education
--- src/data/statistics.js
+++
@@ -1,7 +0,0 @@
-const statistics = [
-    { key: "YEAR OF EXPERIENCE", value: "3+" },
-    { key: "PROJECTS COMPLETED", value: "25+" },
-    { key: "AWARD & RECOGNITION", value: "1" }
-]
-
-export default statistics
