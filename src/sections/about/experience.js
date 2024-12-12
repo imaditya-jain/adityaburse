5529396e8490f3da05bee9f703efe2e35d1058b6
adityajain-web <adityaburse333@gmail.com>
2024-12-12 18:44:31 +0530

    Update experience.js

--- src/sections/about/experience.js
+++ src/sections/about/experience.js
@@ -9,10 +9,12 @@ const Experience = () => {
         <>
             <SectionTitle orangeText="EXPERIENCE" whiteText="MY" titleBg="EXPERIENCE" />
             <Box mt={2}>
-                <Grid container spacing={4} alignItems={"center"}>
+                <Grid container spacing={4}>
                     <Grid item xs={12} md={6}>
-                        <Typography variant='h3' className="text-color" gutterBottom>A Journey Through Professional Development</Typography>
-                        <Typography className="text-color">Embarking on my coding expedition, I've navigated through various roles, each adding a new chapter to my story. From debugging as an intern to seamlessly blending frontend and backend as a Full Stack Developer, every step has been a thrilling revelation. Crafting user interfaces, I've woven digital tapestries that resonate with industry standards, all while maintaining brand consistency. Each role has been a beacon guiding me through the labyrinth of technology, shaping me into the versatile developer I am today.</Typography>
+                        <Box className="sticky-top">
+                            <Typography variant='h3' className="text-color" gutterBottom>A Journey Through Professional Development</Typography>
+                            <Typography className="text-color">Embarking on my coding expedition, I've navigated through various roles, each adding a new chapter to my story. From debugging as an intern to seamlessly blending frontend and backend as a Full Stack Developer, every step has been a thrilling revelation. Crafting user interfaces, I've woven digital tapestries that resonate with industry standards, all while maintaining brand consistency. Each role has been a beacon guiding me through the labyrinth of technology, shaping me into the versatile developer I am today.</Typography>
+                        </Box>
                     </Grid>
                     <Grid item xs={12} md={6}>
                         {
